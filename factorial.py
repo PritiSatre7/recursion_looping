acdfef63a3dba6be3b6ca2499728f0ce79e65923
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-05 22:07:00 +0530

    added INFO and DEBUG logs to track factorial steps

--- factorial.py
+++ factorial.py
@@ -6,11 +6,20 @@ Function for Factorial Numbers
 It will Return the Factorial in loop
 """
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 def factorial(num:int)->int:
+    logger.info("Starting factorial calculation")
+
     retval=1
     while num>1:
+        logger.debug("Multiplying retval with num")
         retval=retval*num
         num=num-1
+        logger.info("Factorial calculation completed")
     return retval
 
 if __name__ == "__main__":
