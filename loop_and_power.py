c18964283081ece57aea02a5aedc6b99406e9c83
PritiSatre7 <priti.satre@vaastav.tech>
2026-02-05 12:37:02 +0530

    write a function for power

--- loop_and_power.py
+++ loop_and_power.py
@@ -1,4 +1,11 @@
 
+def power(num:int, pow:int) -> int:
+    retval = 1
+    while num pow > 0:
+        retval = num*retval
+        pow = pow-1
+    return retval
+
 def repeat_str(text:str , num:int) -> str:
     if num<=0:
         return " "
@@ -8,9 +15,6 @@ def repeat_str(text:str , num:int) -> str:
         return text + repeat_str(text, num-1)
 
 
-
-
-
-
 if __name__ == '__main__':
-    print(repeat_str("Priti", 4))
+
+
