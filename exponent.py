263b8c3d8883fffb0002142a5c4f55dd0cde32ea
PritiSatre7 <priti.satre@vaastav.tech>
2026-02-05 13:03:38 +0530

    write a function for power. loop used

--- exponent.py
+++ exponent.py
@@ -14,5 +14,12 @@ def power(num:int ,pow:int)->int:
     else:
         return num * power(num, pow-1)
 
+def loop_pow(num:int,pow:int)->int:
+    retval = 1
+    while pow > 0:
+        retval= num * retval
+        pow = pow - 1
+    return retval
+
 if __name__ == "__main__":
     print(power(2,3))
