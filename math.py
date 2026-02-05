2c06617bc2c0234ef6524a44be8eb4f4ea3d0a64
PritiSatre7 <priti.satre@vaastav.tech>
2026-02-05 23:38:30 +0530

    write Description for exponential progression

--- math.py
+++ math.py
@@ -1,10 +1,28 @@
 """
+Exponential Progressions
+
+Function for Exponential Progressions
+"""
+
+from exponent import power
+
+
+def exponential( x:int, nterm:int) -> float:
+    retval = 0
+    while nterm > 0:
+        numr = power(x,nterm)
+        denom = fibonacci(nterm)
+        retval = retval + numr/denom
+        nterm = nterm - 1
+    return retval
+
+
+
+"""
 Fibonacci Series
 
 Function for Fibonacci series
 """
-
-
 
 def fibonacci(nterm:int) -> int:
     if nterm <= 1:
@@ -15,3 +33,4 @@ def fibonacci(nterm:int) -> int:
 
 if __name__ == '__main__':
     print(fibonacci(5))
+    print(exponential(2, 3))
