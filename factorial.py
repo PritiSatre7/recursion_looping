43878d1ba52af1ee8f3b0dcf4daddb57bc0a44be
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-05 21:52:56 +0530

    added factorial code

--- factorial.py
+++ factorial.py
@@ -6,4 +6,10 @@ Function for Factorial Numbers
 It will Return the Factorial in loop
 """
 
+def factorial(num:int)->int:
+    retval=1
+    while num>1:
+        retval=retval*num
+        num=num-1
+    return retval
 
