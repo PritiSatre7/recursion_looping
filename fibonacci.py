7df69863892759c0bd1e96a01ee890743cdae029
PritiSatre7 <priti.satre@vaastav.tech>
2026-02-05 23:00:25 +0530

    function for fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+
+
+
+def fibonacci(nterm:int) -> int:
+    if nterm <= 1:
+        return 1
+    else:
+        return fibonacci(nterm-1) + fibonacci(nterm-2)
+
+
+if __name__ == '__main__':
+    print(fibonacci(5))
