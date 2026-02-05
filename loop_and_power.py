0fc0d87aa3407621a0fbdd9016c015f4e93a01b8
PritiSatre7 <priti.satre@vaastav.tech>
2026-02-05 12:30:21 +0530

    write a logic and function called

--- loop_and_power.py
+++ loop_and_power.py
@@ -1,8 +1,16 @@
 
 def repeat_str(text:str , num:int) -> str:
+    if num<=0:
+        return " "
+    elif num==1:
+        return text
+    else:
+        return text + repeat_str(text, num-1)
+
 
 
 
 
 
 if __name__ == '__main__':
+    print(repeat_str("Priti", 4))
