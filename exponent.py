2d1197d4b9f444873f7608d7e9e37f126dd7d6e0
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-05 12:49:21 +0530

    added code to the file

--- exponent.py
+++ exponent.py
@@ -5,3 +5,12 @@
 Function for Exponentional Progressions
 It will return the retval of num and power
 """
+
+def power(num:int ,pow:int)->int:
+    if pow == 0:
+        return 1
+    elif pow == 1:
+        return num
+    else:
+        return num*pow (num,pow-1)
+    
