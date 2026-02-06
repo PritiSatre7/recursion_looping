cdfe4ddba9ef8e17d85e3ea363144eb3f31d7c50
PritiSatre7 <priti.satre@vaastav.tech>
2026-02-06 12:26:57 +0530

    log added

--- fibo.py
+++ fibo.py
@@ -1,12 +1,23 @@
+import math
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 def atarangi(nterm: int) -> int:
-    if nterm ==0:
+    logger.debug("Function is called")
+    if nterm == 0:
+        logger.info("Base case")
         return 0
     elif nterm == 1 or nterm == 2:
+        logger.info("Base case")
         return 1
     else:
+        logger.debug("Calculation Start")
         return 2 * atarangi(nterm-1) - 3 * atarangi(nterm-2) + 4 * atarangi(nterm-3)
+
+
 
 if __name__ == "__main__":
     print(atarangi(4))
