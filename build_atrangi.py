ab67909fe57dc2d316e3546926800a3dec16d790
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-06 13:26:50 +0530

    imported the file atarangi to build_atrangi

--- build_atrangi.py
+++ build_atrangi.py
@@ -5,26 +5,42 @@
 Function for Recurrence Atrangi
 It will return a single integer value for given.
 """
+
+
 import logging
+import fibo
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def atrangi(n: int) -> int:
-    logger.info("Calculating factoraial")
-    fact = 1
-    while n > 0:
-        fact = fact * n
-        logger.debug("Fact updated ")
-        n -= 1
-        logger.info("Factorial result")
-    return fact
+def atarangi(nterm: int) -> int:
+    """
+    get value of atarangi series for a given term
+
+    :param nterm:
+    :return:
+    """
+    logger.debug("Function is called")
+    if nterm == 0:
+        logger.info("Base case")
+        return 0
+    elif nterm == 1 or nterm == 2:
+        logger.info("Base case")
+        return 1
+    else:
+        logger.debug("Calculation Start")
+        return 2 * atarangi(nterm-1) - 3 * atarangi(nterm-2) + 4 * atarangi(nterm-3)
+
 
 def build_atrangi(nterm: int) -> float:
+    """
+       This function calculates the series:
+       1/1! + 2/2! + 3/3! + ... + n/n!
+    """
     logger.info("Building atrangi series for nterm")
     total = 0
     i = 1
     while i <= nterm:
-        total = total + (i / atrangi(i))
+        total = total + (i / atarangi(i))
         logger.info(f"Term (i): (i)/(i)! = value, Total = total")
         i += 1
         logger.info("Final sum")
@@ -35,5 +51,6 @@ if __name__ == "__main__":
     result = build_atrangi(5)
     logger.info("Program finished with result")
     print(build_atrangi(5))
+    print(atarangi(4))
 
 
