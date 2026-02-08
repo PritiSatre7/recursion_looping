9f6d82fbc6e9328b7bfd6a242eb769c5b0edf8cf
PritiSatre7 <priti.satre@vaastav.tech>
2026-02-08 11:13:26 +0530

    logs added

--- check_square.py
+++ check_square.py
@@ -5,15 +5,23 @@ Function checks if the input is a perfect square, if not it falls back to the pr
 
 """
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 def perfect_square(num:int) -> int:
+    logger.info("function started")
     if num < 0:
+        logger.warning("Num is negative, return empty string")
         return " "
     i = 0
     while i * i <= num:
+        logger.debug("checking next square")
         i = i + 1
     if i * i > num:
+        logger.debug("Go one step back")
         i = i - 1
+        logger.info("Returning dot as result")
     return "." * i
 
 if __name__ == "__main__":
