fdd6365cb08ca9063dd232f2e63e69067879f16b
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-09 12:18:52 +0530

    added logging to perfect square iteration

--- check_square.py
+++ check_square.py
@@ -6,18 +6,22 @@ Perfect square
 Function will create a strings and a series based on perfect squares and it will fall back to the previous square when needed.
 """
 
-
+import logging
 import math
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 def perfect_square_iter(ch: str, n: int) -> str:
+    logger.debug("perfect_square_iter having input as character and interger and output as string")
     if n <= 0:
+        logger.debug("n is less than or equal to 0, returning empty string")
         return ""
+
     root = math.isqrt(n)
+    logger.debug(f"integer square root calculated: {root}")
 
-    if root == 0:
-        return ""
-    return ch + perfect_square_iter(ch, (root - 1) * (root - 1))
-
+    return ch * root
 
 if __name__ == "__main__":
     print(perfect_square_iter(".", 0))
